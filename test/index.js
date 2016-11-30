4aa9f858dde791785d3b192b38b188ac88da49b0
StockDC2 <StockDC2@gmail.com>
2016-11-30 03:55:22 -0800

    Add module pattern definition

--- test/index.js
+++ test/index.js
@@ -1,3 +1,6 @@
+// Using module pattern to export an IIFE to provide privacy and state (closure)
+// to variables and methods contained within it.
+
 module.exports = (function() {
     return {
         sanitize: function(word) {
